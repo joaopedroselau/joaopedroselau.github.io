c58142aec163f0d870a66c90dbcbc71c7ea70f48
Maria Eduarda Caetano Gomes <168578666+mimiamimia@users.noreply.github.com>
2024-09-23 17:42:43 -0300

    Atualização 23/09

---
+++ projeto/conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassowrd ='';
+    $dbName = 'cineastro';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassowrd,$dbName);
+?>
+
+
+
+
+
---
+++ projeto/signup.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<link rel="icon" href="assets/cinecon.png" type="image/x-icon">
+	
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="cinema.css">
+	<link rel="stylesheet" href="variables.css">
+	<link rel="stylesheet" href="responsivo.css">
+	<title>Cineastro's cinema</title>
+</head>
+
+<body>
+
+	<header class="cab">
+		<div class="cab__background"></div>
+		<a href="index.html"><img class="cab__image" src="assets/cine.png"></a>
+		<nav class="cab__menu">
+			<ul>
+				<li><a href="index.html" target="_self">CINEASTRO</a></li>
+				<li><a href="programacao.html" target="_self">PROGRAMAÇÃO</a></li>
+				<li><a href="filmes.html" target="_self">FILMES</a></li>
+				<li><a href="noticias.html" target="_self">NOTÍCIAS</a></li>
+				<li><a href="alimentos.html" target="_self">SNACK BAR</a></li>
+				<li><a href="contato.html" target="_self">CONTATO</a></li>
+			</ul>
+		</nav>
+		<nav class="cab__tools">
+            <ul>
+                <li><a href="ajuda/faq.html" target="_self">Ajuda</a></li>
+                <li>|</li>
+                <li><a href="signup.php" target="_self">Cadastro</a></li>
+                <li>|</li>
+                <li><a href="login.php" target="_self">Entrar</a></li>
+			</ul>
+			<div class="cab__social">
+				<a href= "carrinho.html"><img class="cab__social__cart" src="https://pngimg.com/d/shopping_cart_PNG4.png"></a>
+				<a href="https://www.instagram.com/cineastro.taubate/?utm_source=ig_web_button_share_sheet"><img src="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-icon-white-png.png"></a>
+				<a href="https://x.com/Cineastro_tbt"><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/x-social-media-white-icon.png"></a>
+			</div>
+        </nav>
+	</header>
+
+    <div class="form">
+        <fieldset>
+            <legend>Cadastro do cliente</legend>
+            <form method="post" action="form.php">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome">
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email">
+
+                <label for="cpf">CPF:</label>
+                <input type="text" id="cpf" name="cpf" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+
+                <div class="input">
+                    <input type="submit" value="Enviar" name="submit">
+                    <input type="reset" value="Limpar tudo" name="limpar">
+                </div>
+            </form>
+        </fieldset>
+    </div>
+
+
+    <?php
+if (isset($_POST['submit'])) {
+    include_once('conexao.php');
+
+    // Sanitizar e validar entradas
+    $cpf = filter_var($_POST['cpf'], FILTER_SANITIZE_STRING);
+    $nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+    if ($cpf && $nome && $email) {
+        $stmt = $conexao->prepare("INSERT INTO clientes (cpf, nome, email) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cpf, $nome, $email);
+
+        if ($stmt->execute()) {
+            echo "<p id='success-message'> Cadastro realizado com sucesso,</p>";
+        } else {
+            echo "<p id='error-message'> Erro ao realizar cadastro. Por favor, tente novamente.</p>";
+        }
+
+        $stmt->close();
+    } else {
+        echo "<p id='error-message'>Dados inválidos.</p>";
+    }
+
+    $conexao->close();
+}
+?>
+</body>
+</html>
+
+
+
+
